04159dc43755208ca7a1ead774a6e0a7cf1f0bc3
BERRY TLSPL <BERRY TLSPL@DELL>
2026-08-18 17:00:44 +0530

    Update JS hover events and cache versions

--- cache_buster.py
+++ cache_buster.py
@@ -7,11 +7,11 @@ for file in html_files:
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
     
-    # Replace any styles.min.css and styles.min.css?v=XX with styles.min.css?v=20
-    content = re.sub(r'styles\.min\.css(?:\?v=\d+)?', 'styles.min.css?v=20', content)
+    # Replace any styles.min.css and styles.min.css?v=XX with styles.min.css?v=39
+    content = re.sub(r'styles\.min\.css(?:\?v=\d+)?', 'styles.min.css?v=39', content)
     
-    # Replace any main.min.js and main.min.js?v=XX with main.min.js?v=20
-    content = re.sub(r'main\.min\.js(?:\?v=\d+)?', 'main.min.js?v=20', content)
+    # Replace any main.min.js and main.min.js?v=XX with main.min.js?v=39
+    content = re.sub(r'main\.min\.js(?:\?v=\d+)?', 'main.min.js?v=39', content)
     
     with open(file, 'w', encoding='utf-8') as f:
         f.write(content)
